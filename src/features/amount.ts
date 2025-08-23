358589b25594e02847d1f8a55572c229a4649a1a
DatsOlena <datsolena@gmail.com>
2025-08-23 20:45:15 +0200

    Develop amount reducer, install dependencies, add provider

---
+++ src/features/amount.ts
@@ -0,0 +1,53 @@
+const ADD = 'amount/ADD';
+const TAKE = 'amount/TAKE';
+const CLEAR = 'amount/CLEAR';
+
+type AddAmountAction = {
+  type: typeof ADD;
+  payload: number;
+}
+
+const add = (value: number): AddAmountAction => ({
+  type: ADD,
+  payload: value
+});
+
+type TakeAmountAction = {
+  type: typeof TAKE;
+  payload: number;
+}
+
+const take = (value: number): TakeAmountAction => ({
+  type: TAKE,
+  payload: value
+});
+
+type ClearAmountAction = {
+  type: typeof CLEAR;
+}
+
+const clear = (): ClearAmountAction => ({
+  type: CLEAR
+});
+
+type Action = AddAmountAction | TakeAmountAction | ClearAmountAction;
+
+const amountReducer = (amount = 0, action: Action) => {
+  switch (action.type) {
+    case 'amount/ADD':
+      return amount + action.payload;
+    case 'amount/TAKE': {
+      if (amount < action.payload) return amount;
+      return amount - action.payload;
+    }
+
+    case 'amount/CLEAR':
+      return 0;
+    default:
+      return amount;
+  }
+};
+
+export { add, take, clear };
+
+export default amountReducer;

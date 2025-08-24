c6e317796ed3404307287d7a8a435e413655425f
DatsOlena <datsolena@gmail.com>
2025-08-24 21:09:15 +0200

    Implemant custom hook for state typization

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,14 @@
 // npm i redux @types/redux
 import { combineReducers, createStore } from 'redux';
 import amountReducer from '../features/amount';
+import goodsReducer from '../features/goods';
 
 const reducer = combineReducers({
-    amount: amountReducer
+    amount: amountReducer,
+    goods: goodsReducer
 });
 const store = createStore(reducer);
 
-export type RootType = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
